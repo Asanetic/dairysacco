667f8ce6fbe59738cf181ee13786c16e4e3a0487
Asanetic <39214296+Asanetic@users.noreply.github.com>
2025-09-20 13:45:10 +0300

    Initial commit for dairysacco_v1  agent

--- app/mosy_nextjs_boiler/appConfigs/hiveRoutes.jsx
+++
@@ -1,8 +0,0 @@
-// src/app/mosy_utils/hiveRoutes.js
-
-export const hiveRoutes = {
-  hiveBaseRoute: '/dairy',
-  cms: '/dairy/dairysacco',      // relative to your Next.js routes
-  auth: '/dairy/auth'
-  // add more as needed
-};
